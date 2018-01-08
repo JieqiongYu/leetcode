c41b16d273b30f929a79a94cfef8f378f8dbbc4e
Mer <yjieqiong@gmail.com>
2018-01-08 15:35:40 +0900

    20180108

---
+++ Array/SetMatrixZeroes.java
@@ -0,0 +1,57 @@
+/**
+* Given a m x n matrix, if an element is 0, set its entire row and column to 0.
+* Do it in place.
+*
+* Follow up:
+* Did you use extra space?
+* A straight forward solution using O(mn) space is probably a bad idea.
+* A simple improvement uses O(m + n) space, but still not the best solution.
+* Could you devise a constant space solution?
+*
+* https://leetcode.com/problems/set-matrix-zeroes/description/
+*/
+class Solution {
+    public void setZeroes(int[][] matrix) {
+
+        boolean firstRow = false;
+        boolean firstCol = false;
+
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (matrix[i][j] == 0) {
+                    if (i == 0) {
+                        firstRow = true;
+                    }
+                    if (j == 0) {
+                        firstCol = true;
+                    }
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < col; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if (firstRow) {
+            for (int j = 0; j < col; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        if (firstCol) {
+            for (int i = 0; i < row; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
---
+++ String/ValidWordAbbreviation.java
@@ -0,0 +1,46 @@
+/**
+* Given a non-empty string s and an abbreviation abbr, return whether the string
+* matches with the given abbreviation.
+*
+* A string such as "word" contains only the following valid abbreviations:
+* ["word", "1ord", "w1rd", "wo1d", "wor1", "2rd", "w2d", "wo2", "1o1d", "1or1",
+* "w1r1", "1o2", "2r1", "3d", "w3", "4"]
+* Notice that only the above abbreviations are valid abbreviations of the string "word".
+* Any other string is not a valid abbreviation of "word".
+*
+* Note:
+* Assume s contains only lowercase letters and abbr contains only lowercase letters and digits.
+* Example 1:
+* Given s = "internationalization", abbr = "i12iz4n":
+* Return true.
+*
+* Example 2:
+* Given s = "apple", abbr = "a2e":
+* Return false.
+*
+* https://leetcode.com/problems/valid-word-abbreviation/description/
+*/
+class Solution {
+    public boolean validWordAbbreviation(String word, String abbr) {
+
+        int i = 0;
+        int j = 0;
+
+        while (i < word.length() && j < abbr.length()) {
+            if (word.charAt(i) == abbr.charAt(j)) {
+                i++;
+                j++;
+            } else if (abbr.charAt(j) > '0' && abbr.charAt(j) <= '9') {
+                int start = j;
+                while (j < abbr.length() && Character.isDigit(abbr.charAt(j))) {
+                    j++;
+                }
+                i += Integer.valueOf(abbr.substring(start, j));
+            } else {
+                return false;
+            }
+        }
+
+        return i == word.length() && j == abbr.length();
+    }
+}
