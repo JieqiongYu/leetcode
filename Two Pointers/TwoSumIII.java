732ac49e474b7057e768a64b2370e31efdbc9879
Mer <yjieqiong@gmail.com>
2018-02-17 14:07:40 +0900

    Find Smallest Letter Grater Than Target

--- Two Pointers/TwoSumIII.java
+++ Two Pointers/TwoSumIII.java
@@ -1,54 +1,54 @@
 /**
-* Design and implement a TwoSum class.
-* It should support the following operations: add and find.
-*
-* add - Add the number to an internal data structure.
-* find - Find if there exists any pair of numbers which sum is equal to the value.
-*
-* For example,
-* add(1); add(3); add(5);
-* find(4) -> true
-* find(7) -> false
-*
-* https://leetcode.com/problems/two-sum-iii-data-structure-design/description/
-*/
-public class TwoSum {
+ * [LeetCode][170]Two Sum III
+ *
+ * Design and implement a TwoSum class.
+ * It should support the following operations: add and find.
+ * 
+ * add - Add the number to an internal data structure.
+ * find - Find if there exists any pair of numbers which sum is equal to the value.
+ * 
+ * For example,
+ * add(1); add(3); add(5);
+ * find(4) -> true
+ * find(7) -> false
+ * 
+ * https://leetcode.com/problems/two-sum-iii-data-structure-design/description/
+ */
+class TwoSum {
+    
+    List<Integer> list;
+    Map<Integer, Integer> map;
 
-    private List<Integer> list;
-    private Map<Integer, Integer> map;
-
+    /** Initialize your data structure here. */
     public TwoSum() {
         list = new ArrayList<>();
         map = new HashMap<>();
     }
-
-    /*
-     * @param number: An integer
-     * @return: nothing
-     */
+    
+    /** Add the number to an internal data structure.. */
     public void add(int number) {
-        if (map.containsKey(number)) {
-            map.put(number, map.get(number) + 1);
-        } else {
-            map.put(number, 1);
+        map.put(number, map.getOrDefault(number, 0) + 1);
+        if (map.get(number) == 1) {
             list.add(number);
         }
     }
-
-    /*
-     * @param value: An integer
-     * @return: Find if there exists any pair of numbers which sum is equal to the value.
-     */
+    
+    /** Find if there exists any pair of numbers which sum is equal to the value. */
     public boolean find(int value) {
         for (int i = 0; i < list.size(); i++) {
             int num1 = list.get(i);
             int num2 = value - num1;
-            if (num1 == num2 && map.get(num1) > 1 ||
-            num1 != num2 && map.containsKey(num2)) {
+            if (num1 == num2 && map.get(num1) > 1 || num1 != num2 && map.containsKey(num2)) {
                 return true;
             }
         }
-
         return false;
     }
 }
+
+/**
+ * Your TwoSum object will be instantiated and called as such:
+ * TwoSum obj = new TwoSum();
+ * obj.add(number);
+ * boolean param_2 = obj.find(value);
+ */
