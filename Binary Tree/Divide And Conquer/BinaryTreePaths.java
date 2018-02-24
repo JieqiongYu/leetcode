62d884a5ad1977a5ff2a6d93957af610dbd29af7
Mer <yjieqiong@gmail.com>
2018-02-24 23:41:20 +0900

    Binary Tree Paths

--- Binary Tree/Divide And Conquer/BinaryTreePaths.java
+++ Binary Tree/Divide And Conquer/BinaryTreePaths.java
@@ -1,23 +1,26 @@
 /**
-* Given a binary tree, return all root-to-leaf paths.
-*
-* Example
-* Given the following binary tree:
-*
-*    1
-*  /   \
-* 2     3
-*  \
-*   5
-*
-* All root-to-leaf paths are:
-* [
-*  "1->2->5",
-*  "1->3"
-* ]
-* 
-* https://leetcode.com/problems/binary-tree-paths/description/
-*/
+ * 
+ * [LeetCode][257]Binary Tree Paths
+ * 
+ * Given a binary tree, return all root-to-leaf paths.
+ * 
+ * Example
+ * Given the following binary tree:
+ * 
+ *     1
+ *   /   \
+ *  2     3
+ *   \
+ *    5
+ * 
+ *  All root-to-leaf paths are:
+ *  [
+ *   "1->2->5",
+ *   "1->3"
+ *  ]
+ *  
+ *  https://leetcode.com/problems/binary-tree-paths/description/
+ */
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
