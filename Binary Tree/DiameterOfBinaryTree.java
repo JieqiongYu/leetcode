4a26f204d692e7eee8c644423cde8d3a1d42fc52
Mer <yjieqiong@gmail.com>
2018-02-25 16:22:09 +0900

    Diameter of Binary Tree

--- Binary Tree/DiameterOfBinaryTree.java
+++ Binary Tree/DiameterOfBinaryTree.java
@@ -1,4 +1,7 @@
 /**
+ * 
+ * [LeetCode][543]Diameter of Binary Tree
+ * 
  * Given a binary tree, you need to compute the length of the diameter of the tree. 
  * The diameter of a binary tree is the length of the longest path between any two 
  * nodes in a tree. This path may or may not pass through the root.
@@ -19,11 +22,11 @@
  * https://leetcode.com/problems/diameter-of-binary-tree/description/
  */
 /**
- * 定义一个类变量diameter, 保存最大diameter值。
- * 通过递归计算左右子树的深度来计算根节点的diameter(记为temp)，
- * 通过和类变量diameter进行比较，保存较大值。
- * 在每一次递归结束后，返回左右子树的深度，
- * 将二者相加再加2就是根节点的diameter. 
+ * Any path can be written as two arrows (in different directions) from some node, 
+ * where an arrow is a path that starts at some node and only travels from to child nodes. 
+ * 
+ * If we knew the maximum length arrows L, R for each child, then the best path 
+ * touches L + R. 
  */
 /**
  * Definition for a binary tree node.
@@ -34,24 +37,37 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-class Solution {
-    int diameter;
-    public int diameterOfBinaryTree(TreeNode root) {
-        getDepth(root);
-        return diameter;
-    }
-    
-    public int getDepth(TreeNode root) {
-        if (root == null) {
-            return -1;
-        }
-        
-        int left = getDepth(root.left);
-        int right = getDepth(root.right);
-        int temp = left + right + 2;
-        if (diameter < temp) {
-            diameter = temp;
-        }
-        return Math.max(left, right) + 1;
-    }
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+
+	int diameter;
+
+	public int diameterOfBinaryTree(TreeNode root) {
+		getDepth(root);
+		return diameter;
+	}
+
+	private int getDepth(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+
+		int left = getDepth(root.left);
+		int right = getDepth(root.right);
+		int current = left + right;
+
+		if (diameter < current) {
+			diameter = current;
+		}
+
+		return Math.max(left, right) + 1;
+	}
 }
--- Intervals/MeetingRooms.java
+++ Intervals/MeetingRooms.java
@@ -1,4 +1,7 @@
 /**
+ * 
+ * [LeetCode][252]Meeting Rooms
+ * 
  * Given an array of meeting time intervals consisting of start and end times 
  * [[s1,e1],[s2,e2],...] (si < ei), determine if a person could attend all meetings.
  * 
@@ -7,6 +10,12 @@
  * return false.
  * 
  * https://leetcode.com/problems/meeting-rooms/description/
+ */
+/**
+ * Solution:
+ *  The idea here is to sort the meetings by starting time. 
+ *  Then, go through the meetings one by one and make sure that each meeting 
+ *  ends before the next one starts. 
  */
 /**
  * Definition for an interval.
@@ -20,9 +29,9 @@
 class Solution {
     public boolean canAttendMeetings(Interval[] intervals) {
         Arrays.sort(intervals, new Comparator<Interval>(){
-            public int compare(Interval i1, Interval i2) {
-                return i1.start - i2.start;
-            }            
+           public int compare(Interval i1, Interval i2) {
+               return i1.start - i2.start;
+           } 
         });
         
         for (int i = 0; i < intervals.length - 1; i++) {
@@ -34,3 +43,9 @@ class Solution {
         return true;
     }
 }
+/**
+ * Time Complexity: O(nlogn). The time complexity is dominated by sorting.
+ *                  Once the array has been sorted, only O(n) time is taken 
+ *                  to go through the array and determine if there is any overlap.
+ * Space complexity: O(1). Since no additional space is allocated. 
+ */
