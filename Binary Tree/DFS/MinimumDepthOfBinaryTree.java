9429c3a1542965515b928aca4a3df097c5ada1f4
Yu Jieqiong | Mer | PASYD <jieqiong.yu@rakuten.com>
2018-02-26 10:11:44 +0900

    Minimum Depth of Binary Tree

---
+++ Binary Tree/DFS/MinimumDepthOfBinaryTree.java
@@ -0,0 +1,40 @@
+/**
+ * [LeetCode][111]Minimum Depth of Binary Tree. 
+ * 
+ * Given a binary tree, find its minimum depth.
+ * 
+ * The minimum depth is the number of nodes along the shortest path from the root node down to the 
+ * nearest leaf node.
+ * 
+ * https://leetcode.com/problems/minimum-depth-of-binary-tree/description/
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        
+        if (root.left == null) {
+            return minDepth(root.right) + 1;
+        }
+        
+        if (root.right == null) {
+            return minDepth(root.left) + 1;
+        }
+        
+        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    }
+}
+/**
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ */
